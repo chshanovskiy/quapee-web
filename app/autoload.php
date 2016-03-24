c67775f84e3f322f0fa5aee0b2d997d44e57ef61
Chshanovskiy Maxim <chshanovskiy.maxim@gmail.com>
2016-03-24 21:55:59 +0500

    Updated dependencies

--- app/autoload.php
+++ app/autoload.php
@@ -9,7 +9,7 @@ use Dotenv\Dotenv;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-if ((int)getenv('USE_DOTENV') === 1) {
+if ((int)getenv('USE_DOTENV') === 1 || php_sapi_name() === 'cli') {
     $env = new Dotenv(dirname(__DIR__));
     $env->load();
 }
